7b22e5d4e058c0c77e7974fed031b23ec1ea585e
rusber364 <rusber364@gmail.com>
2024-01-29 16:48:20 +0200

    add controllers

---
+++ src/controllers.ts
@@ -0,0 +1,31 @@
+import Router from '@koa/router'
+import { fetchSongTextById } from './fetch-song-text-by-id.ts'
+import { searchByName, type Song } from './search-by-name.ts'
+
+export const router = new Router()
+
+router.get('/:search', async (ctx) => {
+  ctx.body = await searchByName(ctx.params.search)
+})
+
+router.get('/song/:id', async (ctx) => {
+  ctx.body = await fetchSongTextById(`/${ctx.params.id}`)
+})
+
+router.get('/text/:search', async (ctx) => {
+  const searchSongs = await searchByName(ctx.params.search)
+  const songs: Record<string, Song> = {}
+  const listSongs: Promise<Song>[] = []
+
+  searchSongs.forEach(({ id }) => listSongs.push(fetchSongTextById(id)))
+
+  for (let { id, title, lyrics } of await Promise.all(listSongs)) {
+    songs[id] = {
+      id,
+      title,
+      lyrics,
+    }
+  }
+
+  ctx.body = songs
+})
--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,8 @@
 import './config.ts'
-import { fetchSongTextById } from './fetch-song-text-by-id.ts'
-import { searchByName, type Song } from './search-by-name.ts'
 import Koa from 'koa'
-import Router from '@koa/router'
+import { router } from './controllers.ts'
 
 const app = new Koa()
-const router = new Router()
-
-router.get('/:search', async (ctx) => {
-  ctx.body = await searchByName(ctx.params.search)
-})
-
-router.get('/text/:search', async (ctx) => {
-  const searchSongs = await searchByName(ctx.params.search)
-  const songs: Record<string, Song> = {}
-  const listSongs: Promise<Song>[] = []
-
-  searchSongs.forEach(({ id }) => listSongs.push(fetchSongTextById(id)))
-
-  for (let { id, title, lyrics } of await Promise.all(listSongs)) {
-    songs[id] = {
-      id,
-      title,
-      lyrics,
-    }
-  }
-
-  ctx.body = songs
-})
-
-router.get('/song/:id', async (ctx) => {
-  const song: Song = await fetchSongTextById(`/${ctx.params.id}`)
-
-  ctx.body = song
-})
 
 app.use(router.routes()).use(router.allowedMethods())
 app.listen(3000, () => console.log('The server is running on port 3000'))
