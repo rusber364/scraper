8c85809df2f09c4fd6618960415785f8863f1beb
rusber364 <rusber364@gmail.com>
2024-02-02 18:05:30 +0200

    add parsePsalms

--- src/utils/convert-song.ts
+++ src/utils/convert-song.ts
@@ -1,34 +1,15 @@
 import fs from 'node:fs/promises'
 import path from 'node:path'
 import process from 'node:process'
-import { md5 } from 'js-md5'
-import type { Song } from '../api/search-by-name.ts'
-import { lineCapitalized } from './capitalize.ts'
+import { parsePsalms } from './parse-psalms.ts'
 
 try {
   const [fileName] = process.argv.slice(2)
   const currentFile = path.resolve(process.cwd(), fileName)
   const songs = await fs.readFile(currentFile)
   const newPathFile = currentFile.replace('.sog', '.json')
-  const parsedSongs: Record<string, Song> = {}
 
-  songs
-    .toString('utf8')
-    .toLowerCase()
-    .split('\r\n\r\n')
-    .forEach((song, idx) => {
-      const songLines = song.split('\r\n')
-      const [_, title] = songLines.splice(0, 2)
-      const id = md5(`${title}-${idx + 100_000}`)
-
-      parsedSongs[id] = {
-        id,
-        title: lineCapitalized(title),
-        lyrics: songLines.map(lineCapitalized).join('\n'),
-      }
-    })
-
-  fs.writeFile(newPathFile, JSON.stringify(parsedSongs, null, 2))
+  fs.writeFile(newPathFile, JSON.stringify(parsePsalms(songs), null, 2))
 } catch (error) {
   console.error(error)
   throw new Error((error as Error).message)
---
+++ src/utils/parse-psalms.ts
@@ -0,0 +1,25 @@
+import { md5 } from 'js-md5'
+import type { Song } from '../api/search-by-name.ts'
+import { lineCapitalized } from './capitalize.ts'
+
+export function parsePsalms(psalms: Buffer) {
+  const parsedSongs: Record<string, Song> = {}
+
+  psalms
+    .toString('utf8')
+    .toLowerCase()
+    .split('\r\n\r\n')
+    .forEach((song, idx) => {
+      const songLines = song.split('\r\n')
+      const [_, title] = songLines.splice(0, 2)
+      const id = md5(`${title}-${idx + 100_000}`)
+
+      parsedSongs[id] = {
+        id,
+        title: lineCapitalized(title),
+        lyrics: songLines.map(lineCapitalized).join('\n'),
+      }
+    })
+
+  return parsedSongs
+}
